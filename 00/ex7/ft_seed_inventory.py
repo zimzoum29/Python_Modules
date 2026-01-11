c3c6e5424ed0b7362091887f05e703cd71a39c50
zimzoum29 <timeo.gondran@hotmail.com>
2026-01-11 18:55:08 +0100

    Module 00 finish

--- 00/ex7/ft_seed_inventory.py
+++ 00/ex7/ft_seed_inventory.py
@@ -1,1 +1,9 @@
-def
+def ft_seed_inventory(seed_name, quantity, unit):
+    seed = seed_name.capitalize()
+
+    if unit == "packets":
+        print(f"{seed} seeds: {quantity} packets available")
+    elif unit == "grams":
+        print(f"{seed} seeds: {quantity} grams total")
+    elif unit == "area":
+        print(f"{seed} seeds: covers {quantity} square meters")
