bee5ba7157556e85ae7332fb6f08aa6684de1ae3
zimzoum29 <timeo.gondran@hotmail.com>
2026-02-24 15:29:45 +0100

    fix module 4

--- 04/ex2/ft_stream_management.py
+++ 04/ex2/ft_stream_management.py
@@ -7,8 +7,10 @@ def main() -> None:
     archivist_id: str = input("Input Stream active. Enter archivist ID: ")
     status: str = input("Input Stream active. Enter status report: ")
 
-    sys.stdout.write(f"[STANDARD] Archive status from {archivist_id}: {status}\n")
-    sys.stderr.write("[ALERT] System diagnostic: Communication channels verified\n")
+    sys.stdout.write("[STANDARD] Archive status from ")
+    sys.stdout.write(f"{archivist_id}: {status}\n")
+    sys.stderr.write("[ALERT] System diagnostic: ")
+    sys.stderr.write("Communication channels verified\n")
     sys.stdout.write("[STANDARD] Data transmission complete\n")
     sys.stdout.write("Three-channel communication test successful.\n")
 
